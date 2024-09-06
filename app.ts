6db9ddbd463fac03b90fcafc411ae3773f10e449
pizzou <pmpumuropizzou@gmail.com>
2024-09-06 17:45:19 +0200

    008

--- app.ts
+++ app.ts
@@ -18,20 +18,30 @@ app.use(express.json({ limit: "50mb" }));
 // cookie parser
 // app.use(cookieParser());
 
-const allowedOrigins = ['http://localhost:3000', 'your-production-frontend-url'];
+const allowedOrigins = ['http://localhost:3000',];
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigins.includes(req.headers.origin as string) ? req.headers.origin : '');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Handle preflight OPTIONS request
+  }
+  next();
+});
 
 app.use(cors({
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    // If origin is undefined (e.g., non-browser requests) or is in allowedOrigins
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);  // Allow the request
+      callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // Allow cookies or session-based auth
+  credentials: true, // Required if you need to send cookies or Authorization headers
 }));
 
 
