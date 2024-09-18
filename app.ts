138919f30acdc1f61e7be57611a0bf55d58ff750
pizzou <pmpumuropizzou@gmail.com>
2024-09-18 10:27:20 +0200

    commit 02

--- app.ts
+++ app.ts
@@ -17,36 +17,40 @@ export const app = express();
 // CORS configuration
 const corsOptions = {
   origin: 'https://client-beta-navy.vercel.app', // Allow only your Vercel client
-  credentials: true, // Enable credentials if necessary
+  credentials: true, // Enable credentials to support cookies
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  optionsSuccessStatus: 200, // For legacy browsers
 };
 
 app.use(cors(corsOptions));
-// Body parser
+
+// Body parser for handling JSON payloads
 app.use(express.json({ limit: "50mb" }));
 
-// Cookie parser
+// Cookie parser for handling cookies
 app.use(cookieParser());
 
-// Rate limiting middleware
+// Rate limiting middleware to limit requests from the same IP
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  max: 100, // Limit each IP to 100 requests per window
+  standardHeaders: true, // Return rate limit info in headers
+  legacyHeaders: false, // Disable legacy headers
 });
 
 // Apply the rate limiter to all requests
 app.use(limiter);
 
-// Routes
+// Routes for the various resources in the application
 app.use("/api/v1", userRouter, orderRouter, courseRouter, notificationRouter, analyticsRouter, layoutRouter);
 
-// Handle unknown routes
+// Handle unknown routes with a 404 error
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
-  const err = new Error(`Route ${req.originalUrl} not found`)as any;
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
   next(err);
 });
 
-// Middleware to handle errors
+// Middleware to handle errors globally
 app.use(ErrorMiddleware);
