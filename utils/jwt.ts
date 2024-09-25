0fc8011f08c20f08ba6aa3dd4a20b42f66134454
pizzou <pmpumuropizzou@gmail.com>
2024-09-25 15:34:16 +0200

    commit 001

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -39,7 +39,11 @@ export const sendToken = async (user: IUser, statusCode: number, res: Response) 
 
     // Set tokens in cookies
     res.cookie("access_token", accessToken, accessTokenOptions);
-    res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+console.log("Access Token Set:", accessToken);
+console.log("Refresh Token Set:", refreshToken);
+
 
     // Send response with success and user data
     res.status(statusCode).json({
